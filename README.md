d23de1d682d7f2827339f3b6809d23c4198151d3
paulacamargo25 <paulitacv25@gmail.com>
2018-09-19 19:28:20 -0500

    Initialize the project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# closer
+CLoserApp
---
+++ app/src/main/java/com/example/paula/closerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paula.closerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
