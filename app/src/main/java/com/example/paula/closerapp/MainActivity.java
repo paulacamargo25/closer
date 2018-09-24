3921b2efd4fb4a801eb5ca3fb11ce52104ddc3a6
angeljsd <angeljose9629@gmail.com>
2018-09-24 07:22:16 -0500

    map added

--- app/src/main/java/com/example/paula/closerapp/MainActivity.java
+++ app/src/main/java/com/example/paula/closerapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.paula.closerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button ButtonMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonMap = findViewById(R.id.buttonMap);
+        ButtonMap.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch (view.getId()){
+            case R.id.buttonMap:
+                i = new Intent(this, MapsActivity.class); startActivity(i); break;
+        }
     }
 }
