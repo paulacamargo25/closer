19a153e9cda5a4091dc5a8ed10ea5099a3a3a454
angeljsd <angeljose9629@gmail.com>
2018-10-03 18:48:04 -0500

    places

--- app/src/main/java/com/example/paula/closerapp/MainActivity.java
+++ app/src/main/java/com/example/paula/closerapp/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.paula.closerapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.util.Log;
+
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button ButtonMap;
+    private TextView LocationText;
+    private ListView PlacesListView;
     private double longitude;
     private double latitude;
+
+    public static final String TAG = "CurrentLocNearByPlaces";
+    private static final int LOC_REQ_CODE = 1;
+
+    protected GeoDataClient geoDataClient;
+    protected PlaceDetectionClient placeDetectionClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +54,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButtonMap = findViewById(R.id.buttonMap);
         ButtonMap.setOnClickListener(this);
+        LocationText = findViewById(R.id.location_tv);
+        PlacesListView = findViewById(R.id.list_places);
 
         // Acquire a reference to the system Location Manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
 
         // Define a listener that responds to location updates
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 //makeUseOfNewLocation(location);
+
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -59,7 +91,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        Location last = locationManager.getLastKnownLocation(locationProvider);
+        LocationText.setText(last.toString());
+        getActivity();
 
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getActivity() {
+        Task<PlaceLikelihoodBufferResponse> placeResult = placeDetectionClient.
+                getCurrentPlace(null);
+        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                Log.d(TAG, "current location places info");
+                List<String> placesList = new ArrayList<>();
+                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    placesList.add(placeLikelihood.getPlace().freeze().getName().toString());
+                }
+                likelyPlaces.release();
+                ListAdapter arrayAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.activity_listview, R.id.textView, placesList);
+                PlacesListView.setAdapter(arrayAdapter);
+
+            }
+        });
     }
 
 
